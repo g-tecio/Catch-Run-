f7c568e0eb9ca643267549ad90037a917308e1c9
sirzsgt <diego.aragon@g-tec.io>
2018-11-29 10:16:14 -0600

    First Commit

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+	public float moveSpeed = 300;
+	public GameObject character;
+
+	private Rigidbody2D characterBody;
+	private float ScreenWidth;
+
+	void Start () {
+		ScreenWidth = Screen.width;
+		characterBody = character.GetComponent<Rigidbody2D>();
+	}
+	
+
+	void Update () {
+		int i = 0;
+		while (i < Input.touchCount)
+		{
+			if(Input.GetTouch (i).position.x > ScreenWidth/2){
+				RunCharacter(2.0f);
+			}
+			if(Input.GetTouch (i).position.x < ScreenWidth/2){
+				RunCharacter(-2.0f);
+			}
+		}
+	}
+
+	void FixedUpdate() {
+		#if UNITY_EDITOR
+		RunCharacter(Input.GetAxis("Horizontal"));
+		#endif
+	}
+
+	private void RunCharacter(float horizontalInput){
+		characterBody.AddForce(new Vector2(horizontalInput * moveSpeed * Time.deltaTime, 0));
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+	Rigidbody2D player;
+
+	bool grounded;
+	public float jumpforce;
+	public float dirX = 100f;
+	public float dirLessX = -100f;
+	public float moveSpeed = 300;
+	bool touchButton = false;
+
+	void Start () {
+		player = GetComponent<Rigidbody2D>();
+	}
+	
+	public void Jump(){
+		if(grounded == true){
+			player.AddForce(Vector2.up * jumpforce);
+			grounded = false;
+		}
+	}
+
+	// public void MovePlayerRight(){
+	// 	if(touchButton == false){
+	// 		player.AddForce(new Vector2(dirX * moveSpeed * Time.deltaTime, 0));
+	// 		touchButton = true;
+	// 	}
+	// }
+
+	// public void MovePlayerLeft(){
+	// 	if(touchButton == true){
+	// 		player.AddForce(new Vector2(dirLessX * moveSpeed * Time.deltaTime, 0));
+	// 		touchButton = false;
+	// 	}
+	// }
+
+	private void OnCollisionEnter2D(Collision2D other) {
+		if(other.gameObject.tag == "Bottom")
+		{
+			grounded = true;
+		}
+	}
+	
+
+}
