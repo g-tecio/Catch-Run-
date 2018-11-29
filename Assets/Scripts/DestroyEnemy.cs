f36d983743f010ceba0ea2805475e4898ae3d416
sirzsgt <diego.aragon@g-tec.io>
2018-11-29 15:09:45 -0600

    Gameplay

---
+++ Assets/Scripts/DestroyEnemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DestroyEnemy : MonoBehaviour {
+
+	public GameObject DeathEffectObj;
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		if (other.gameObject.tag == "Player")
+    	{
+
+    		GameObject effectObj = Instantiate(DeathEffectObj, other.contacts[0].point, Quaternion.identity);
+    		Destroy(effectObj, 1.5f);
+    		Destroy(other.gameObject);
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    	}
+	}
+}
---
+++ Assets/Scripts/RandomSpawn.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSpawn : MonoBehaviour {
+
+	public GameObject prefab1, prefab2, prefab3;
+
+	public float spawnRate = 2f;
+
+	float nextSpawn = 1f;
+
+	int whatToSpawn;
+	
+	void Update () {
+		if (Time.time > nextSpawn)
+		{
+			whatToSpawn = Random.Range(1, 6);
+
+			switch (whatToSpawn)
+			{
+				case 1: 
+					Instantiate(prefab1, transform.position, Quaternion.identity);
+					break;
+				case 2: 
+					Instantiate(prefab2, transform.position, Quaternion.identity);
+					break;
+				case 3: 
+					Instantiate(prefab3, transform.position, Quaternion.identity);
+					break;
+			}
+			nextSpawn = Time.time + spawnRate; 
+		}
+	}
+}
