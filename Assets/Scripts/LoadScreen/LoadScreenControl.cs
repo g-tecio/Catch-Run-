3629c0522474215c37f71deff429c64b731d9957
sirzsgt <diego.aragon@g-tec.io>
2018-12-05 12:44:37 -0600

    CareUp

---
+++ Assets/Scripts/LoadScreen/LoadScreenControl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class LoadScreenControl : MonoBehaviour {
+
+	public GameObject loadScreenObj;
+	public Slider slider;
+	public TextMeshProUGUI progressText;
+	
+
+	public void LoadScreen(int sceneIndex){
+		StartCoroutine(LoadingScreen(sceneIndex));
+	}
+
+	IEnumerator LoadingScreen(int sceneIndex){
+		
+		AsyncOperation async = SceneManager.LoadSceneAsync(sceneIndex);
+		loadScreenObj.SetActive(true);
+		while (!async.isDone)
+		{
+			float progress = Mathf.Clamp01(async.progress / .9f);
+			slider.value = progress;
+			progressText.text = progress * 100f + "%";
+
+			yield return null;
+		}
+
+
+	}
+}
