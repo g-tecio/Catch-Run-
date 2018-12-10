34f0cb6cdf386585235cf4186fa7bb959e5888c9
sirzsgt <diego.aragon@g-tec.io>
2018-12-10 14:48:36 -0600

    Max Coins

---
+++ Assets/Scripts/LoadScreen/LoadStatsControl.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadStatsControl : MonoBehaviour {
+
+	public void MenuAction(){
+		SceneManager.LoadScene(3);
+	}
+}
