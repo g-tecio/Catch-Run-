ac121da6e1183b80a45ee81f6eaec780a1594d60
sirzsgt <diego.aragon@g-tec.io>
2018-12-14 12:57:24 -0600

    CareUp 14/12/2018

--- Assets/Scripts/GameScripts/ScoreManager.cs
+++ Assets/Scripts/GameScripts/ScoreManager.cs
@@ -5,6 +5,13 @@ using TMPro;
 
 public class ScoreManager : MonoBehaviour {
 
+	public GameObject spawner1;
+    public GameObject spawner2;
+    public GameObject spawner3;
+	public GameObject spawner5;
+	public GameObject spawner6;
+	public GameObject spawner7;
+
 	public TextMeshProUGUI currentScoreText;
 	public TextMeshProUGUI bestScoreText;
 	int currentScore = 0;
@@ -28,5 +35,24 @@ public class ScoreManager : MonoBehaviour {
 			bestScoreText.text = currentScore.ToString();
 			PlayerPrefs.SetInt("BestScore", currentScore);
 		}
+
+		// if(currentScore == 1){
+		// spawner1.SetActive(true);
+		// }
+		// if(currentScore == 2){
+		// 	spawner2.SetActive(true);
+		// }
+		// if(currentScore == 3){
+		// 	spawner3.SetActive(true);
+		// }
+		// if(currentScore == 5){
+		// 	spawner5.SetActive(true);
+		// }
+		// if(currentScore == 6){
+		// 	spawner6.SetActive(true);
+		// }
+		// if(currentScore == 7){
+		// 	spawner7.SetActive(true);
+		// }
 	}
 }
