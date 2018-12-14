4a07a06d97b8d554b9d9d7913321345a5071b579
sirzsgt <diego.aragon@g-tec.io>
2018-12-14 14:00:17 -0600

    Portals

---
+++ Assets/Scripts/GameScripts/EnterPortal.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnterPortal : MonoBehaviour {
+
+	public GameObject portal;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if(other.gameObject.tag == "Player"){
+			other.transform.position = portal.transform.position;
+		}
+	}
+}
